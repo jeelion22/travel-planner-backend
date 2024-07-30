0a9cfd92341572e74e26cbb68ff7bae9af260910
jeeva m <jeelion22@gmail.com>
2024-07-30 22:29:34 +0530

    added routes

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -1,14 +1,23 @@
-const express = require("express")
+const express = require("express");
 
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
 
-const tripController = require("../controllers/tripController")
+const tripController = require("../controllers/tripController");
 
 const tripRouter = express.Router();
 
-tripRouter.post("/addTrip", auth.isAuth, tripController.addTrip)
-tripRouter.get("/allTrips", auth.isAuth, tripController.getAllTripsByUserId)
-tripRouter.put("/trips/:tripId", auth.isAuth, tripController.editTrip)
-tripRouter.delete("/trips/:tripId", auth.isAuth, tripController.deleteTrip)
-tripRouter.put("/trips/addBudget/:tripId", auth.isAuth, tripController.addBudget)
-module.exports = tripRouter
+tripRouter.post("/addTrip", auth.isAuth, tripController.addTrip);
+tripRouter.get("/allTrips", auth.isAuth, tripController.getAllTripsByUserId);
+tripRouter.put("/trips/:tripId", auth.isAuth, tripController.editTrip);
+tripRouter.delete("/trips/:tripId", auth.isAuth, tripController.deleteTrip);
+tripRouter.put(
+  "/trips/addBudget/:tripId",
+  auth.isAuth,
+  tripController.addBudget
+);
+tripRouter.put(
+  "/trips/travels/booking/:tripId",
+  auth.isAuth,
+  tripController.bookTravel
+);
+module.exports = tripRouter;
