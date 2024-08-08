cd494eb5f2048918c73b11647342a8ad1c5e0108
jeeva m <jeelion22@gmail.com>
2024-08-08 17:45:51 +0530

    added train type model

---
+++ models/travelType.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const TravelBooking = require("./travelBooking");
+
+const flightBookingSchema = new mongoose.Schema({
+  airline: {
+    type: String,
+    required: [true, "Airline required for flight"],
+  },
+  flightNumber: {
+    type: String,
+    required: [true, "Flight number required for flight"],
+  },
+});
+
+const FlightBooking = TravelBooking.discriminator(
+  "flight",
+  flightBookingSchema
+);
+
+const trainBookingSchema = new mongoose.Schema({
+  trainNumber: {
+    type: String,
+    required: [true, "Train number required"],
+  },
+  coach: {
+    type: String,
+    required: [true, "Coach information required for train"],
+  },
+});
+
+const TrainBooking = TravelBooking.discriminator("train", trainBookingSchema);
+
+const busBookingSchema = new mongoose.Schema({
+  buscompany: {
+    type: String,
+    required: [true, "Bus company required for bus"],
+  },
+  busNumber: {
+    type: String,
+    required: [true, "Bus number required for bus"],
+  },
+});
+
+const BusBooking = TravelBooking.discriminator("bus", busBookingSchema);
+
+const carRentalBookingSchema = new mongoose.Schema({
+  rentalCompany: {
+    type: String,
+    required: [true, "Car model required for car rental"],
+  },
+  carModel: {
+    type: String,
+    required: [true, "Car model required for car rental"],
+  },
+  carNumber: {
+    type: String,
+    required: [true, "Car number required for car rental"],
+  },
+});
+
+const CarRentalBooking = TravelBooking.discriminator(
+  "car rental",
+  carRentalBookingSchema
+);
+
+const otherBookingSchema = new mongoose.Schema({
+  details: {
+    type: String,
+    required: [true, "Detauks required for other travel type"],
+  },
+});
+
+const OtherBooking = TravelBooking.discriminator("other", otherBookingSchema);
+
+module.exports = {
+  FlightBooking,
+  TrainBooking,
+  BusBooking,
+  CarRentalBooking,
+  OtherBooking,
+};
