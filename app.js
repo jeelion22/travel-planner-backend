325a498f99e5f693ff649f87069cc945a4f38084
jeeva madhaiyan <jeelion22@gmail.com>
2024-09-05 16:45:10 +0530

    head method changed to get

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.head("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Welcome to the Travel Planner API Endpoints!");
 });
 
